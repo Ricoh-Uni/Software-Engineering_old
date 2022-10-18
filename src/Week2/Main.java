ab3486b1ff4a63e4ad1d01f78667ebd483fe5a7f
Ricoh-Uni <ricoh.handt@ruhr-uni-bochum.de>
2022-10-18 20:44:58 +0200

    Created week 2 and wrote the first code

---
+++ src/Week2/Main.java
@@ -0,0 +1,16 @@
+package Week2;
+
+import java.util.date;
+
+public class Main {
+    void main();
+    public class Student {
+        String firstname;
+        String lastname;
+        long studentId;
+        double weight;
+        Date birthday;
+
+        public enum SortKey {FIRSTNAME, LASTNAME, STUDENT_ID, WEIGHT, BIRTHDAY}
+    }
+}
